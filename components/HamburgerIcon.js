58b4b28f84ea738bc54e921ba3b6e288106618f8
Unto Pulkkinen <untop@k-d557-mac-15.pk.amk.root>
2023-09-04 13:18:50 +0300

    added bodymovin icon to app

--- components/HamburgerIcon.js
+++ components/HamburgerIcon.js
@@ -1,11 +1,33 @@
+import LottieView from 'lottie-react-native';
 import PropTypes from 'prop-types';
-import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Hamburger from '../Lottie/Hamburger.json'
+import HamburgerReverse from '../Lottie/HamburgerReverse.json'
+import { useEffect, useState } from 'react';
+import { useDrawerStatus } from '@react-navigation/drawer';
 
 const HamburgerIcon = ({ navigation }) => {
+
+  const [anim, setAnim] = useState(Hamburger);
+
+  const isDrawerOpen = useDrawerStatus() !== 'open';
+
+  const pressHandler = () => {
+    navigation.toggleDrawer();
+  }
+
+  useEffect(()=>{
+    isDrawerOpen ? setAnim(HamburgerReverse) : setAnim(Hamburger);
+  }, [isDrawerOpen]);
+
   return (
-    <View>
-      <Text>HamburgerIcon</Text>
-    </View>
+    <TouchableOpacity onPress={pressHandler}>
+      <LottieView
+        source={anim}
+        style={{ height: 40, width: 40, marginLeft: 10 }}
+        loop ={false}
+        autoPlay = {true} />
+    </TouchableOpacity>
   );
 };
 
